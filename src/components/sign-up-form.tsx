3106b7ffbeaa22c34df46d8f241d68ba37331161
mar-alv <mar.alv.cod@gmail.com>
2024-09-04 19:10:26 -0300

    feat: email field

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -13,7 +13,8 @@ interface Props {
 
 const signUpSchema = z.object({
   firstName: z.string().min(1).regex(/\S+/),
-  lastName: z.string().min(1).regex(/\S+/)
+  lastName: z.string().min(1).regex(/\S+/),
+	email: z.string().email()
 })
 
 type SignUpSchema = z.infer<typeof signUpSchema>
@@ -29,6 +30,7 @@ export function SignUpForm({ signUp }: Props) {
 
     formData.append('firstName', data.firstName)
     formData.append('lastName', data.lastName)
+    formData.append('email', data.email)
 
 		await signUp(formData)
   }
@@ -49,7 +51,7 @@ export function SignUpForm({ signUp }: Props) {
 						type='text'
 						className={clsx(
 							'w-full p-2 rounded-[4px] outline outline-1 outline-gray-500 text-gray-900 peer',
-							'data-[invalid=true]:outline-[3px] data-[invalid=true]:outline-rose-600',
+							errors.firstName && 'outline-[3px] outline-rose-600',
 							'placeholder:text-gray-300',
 							'focus:outline-2 focus:outline-gray-900 dark:focus:outline-offset-2 dark:focus:outline-gray-100'
 						)}
@@ -76,7 +78,7 @@ export function SignUpForm({ signUp }: Props) {
 						type='text'
 						className={clsx(
 							'w-full p-2 rounded-[4px] outline outline-1 outline-gray-500 text-gray-900 peer',
-							'data-[invalid=true]:outline-[3px] data-[invalid=true]:outline-rose-600',
+							errors.lastName && 'outline-[3px] outline-rose-600',
 							'placeholder:text-gray-300',
 							'focus:outline-2 focus:outline-gray-900 dark:focus:outline-offset-2 dark:focus:outline-gray-100'
 						)}
@@ -91,6 +93,32 @@ export function SignUpForm({ signUp }: Props) {
 				</Form.Field>
 
 			</div>
+
+			<Form.Field name='email' className='gap-2 grid content-start'>
+				<Form.Label className='justify-self-start text-xs peer-focus:font-bold'>
+					Email
+				</Form.Label>
+
+				<Form.Control
+					maxLength={150}
+					placeholder='johndoe@mail.com'
+					required
+					type='email'
+					className={clsx(
+						'w-full p-2 rounded-[4px] outline outline-1 outline-gray-500 text-gray-900 peer',
+						errors.email && 'outline-[3px] outline-rose-600',
+						'placeholder:text-gray-300',
+						'focus:outline-2 focus:outline-gray-900 dark:focus:outline-offset-2 dark:focus:outline-gray-100'
+					)}
+					{...register('email')}
+				/>
+
+				{errors.email && (
+					<Form.Message className='text-rose-600'>
+						Please enter your email.
+					</Form.Message>
+				)}
+			</Form.Field>
 
       <Form.Submit className='py-2 rounded-lg text-white bg-red-400 hover:bg-rose-600'>
         Sign up
